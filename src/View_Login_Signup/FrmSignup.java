2870ac3cec4bf944080ffe60715fe0ddb40a8984
Lienn12 <liennguyen4221@gmail.com>
2024-10-28 01:07:42 +0700

    remove main and update signup

--- src/View_Login_Signup/FrmSignup.java
+++ src/View_Login_Signup/FrmSignup.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package View_Admin;
+package View_Login_Signup;
 
 import Controllers.User_controller;
 import java.sql.SQLException;
